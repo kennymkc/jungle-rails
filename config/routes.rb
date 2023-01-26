af2ea7981f6244f42c609d39ff0b2e81ee815918
Kennie <Kenneth Chan>
2023-01-25 21:47:49 -0500

    implement new feature: About page add controller, index & route to show on top & bottom navbars

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
