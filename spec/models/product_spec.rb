fc624e58e031c8dfcb57d9a5bc0485a4e4abe45d
Kennie <Kenneth Chan>
2023-01-30 14:45:11 -0500

    verify each validation is correct with the right error message

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -20,5 +20,29 @@ RSpec.describe Product, type: :model do
       expect(subject).to be_valid
     end
 
+    it 'is not valid when name is nil' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'is not valid when price is nil' do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Price can't be blank")
+    end
+
+     it 'is not valid when quantity is nil' do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'is not valid when category is nil' do
+      subject.category = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Category can't be blank")
+    end
+
   end
 end
